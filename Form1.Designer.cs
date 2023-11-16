b06806ba417d71f84f8c6f2c39c5d3c1aac1305a
Leslie G <giselle.romero1999@gmail.com>
2023-11-15 18:13:08 -0800

    Tarea27

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Tarea_27
+{
+    partial class fmrTarea27
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumAl = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.cmdVer = new System.Windows.Forms.Button();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.lblIntentos = new System.Windows.Forms.Label();
+            this.cmdOtro = new System.Windows.Forms.Button();
+            this.cmdSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumAl
+            // 
+            this.lblNumAl.AutoSize = true;
+            this.lblNumAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumAl.Location = new System.Drawing.Point(21, 37);
+            this.lblNumAl.Name = "lblNumAl";
+            this.lblNumAl.Size = new System.Drawing.Size(461, 24);
+            this.lblNumAl.TabIndex = 0;
+            this.lblNumAl.Text = "Estoy pensando en un número del 1 al 100, ¿cuál es?";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum.Location = new System.Drawing.Point(100, 76);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 26);
+            this.txtNum.TabIndex = 1;
+            // 
+            // cmdVer
+            // 
+            this.cmdVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdVer.Location = new System.Drawing.Point(221, 76);
+            this.cmdVer.Name = "cmdVer";
+            this.cmdVer.Size = new System.Drawing.Size(79, 26);
+            this.cmdVer.TabIndex = 2;
+            this.cmdVer.Text = "&Ver";
+            this.cmdVer.UseVisualStyleBackColor = true;
+            this.cmdVer.Click += new System.EventHandler(this.cmdVer_Click);
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensaje.Location = new System.Drawing.Point(12, 167);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(123, 20);
+            this.lblMensaje.TabIndex = 3;
+            this.lblMensaje.Text = "alksdhfalksdhfjh";
+            // 
+            // lblIntentos
+            // 
+            this.lblIntentos.AutoSize = true;
+            this.lblIntentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIntentos.Location = new System.Drawing.Point(12, 198);
+            this.lblIntentos.Name = "lblIntentos";
+            this.lblIntentos.Size = new System.Drawing.Size(123, 20);
+            this.lblIntentos.TabIndex = 4;
+            this.lblIntentos.Text = "alksdhfalksdhfjh";
+            // 
+            // cmdOtro
+            // 
+            this.cmdOtro.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdOtro.Location = new System.Drawing.Point(306, 76);
+            this.cmdOtro.Name = "cmdOtro";
+            this.cmdOtro.Size = new System.Drawing.Size(79, 26);
+            this.cmdOtro.TabIndex = 5;
+            this.cmdOtro.Text = "&Otro";
+            this.cmdOtro.UseVisualStyleBackColor = true;
+            this.cmdOtro.Click += new System.EventHandler(this.cmdOtro_Click);
+            // 
+            // cmdSalir
+            // 
+            this.cmdSalir.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSalir.Location = new System.Drawing.Point(391, 76);
+            this.cmdSalir.Name = "cmdSalir";
+            this.cmdSalir.Size = new System.Drawing.Size(79, 26);
+            this.cmdSalir.TabIndex = 6;
+            this.cmdSalir.Text = "&Salir";
+            this.cmdSalir.UseVisualStyleBackColor = true;
+            this.cmdSalir.Click += new System.EventHandler(this.cmdSalir_Click);
+            // 
+            // fmrTarea27
+            // 
+            this.AcceptButton = this.cmdVer;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.CancelButton = this.cmdSalir;
+            this.ClientSize = new System.Drawing.Size(494, 265);
+            this.Controls.Add(this.cmdSalir);
+            this.Controls.Add(this.cmdOtro);
+            this.Controls.Add(this.lblIntentos);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.cmdVer);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.lblNumAl);
+            this.Name = "fmrTarea27";
+            this.Text = "Adivina  número";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumAl;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button cmdVer;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Label lblIntentos;
+        private System.Windows.Forms.Button cmdOtro;
+        private System.Windows.Forms.Button cmdSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_27
+{
+    public partial class fmrTarea27 : Form
+    {
+        public fmrTarea27()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdVer_Click(object sender, EventArgs e)
+        {
+            // Programa que permita a un jugador adivinar un número indeterminado de veces que
+            // consiste en lo siguiente:
+            // A. Genera un número aleatorio comprendido entre 0-100
+            // B. Permite al jugador adivinar el número generado, permitiéndoles introducir por
+            // teclado número mientras que no acierte. Con cada número introducido se le informará
+            // por pantalla si el número generado es mayor o menor.
+            // C. El programa cuenta el npumero de intentos y muestra al jugador en pantalla al
+            // momento de la victoria, con el número de intentos. Si el número de intentos es 
+            // menor o igual a 5 se indica al jugador que es bueno, si es mayor a 5 y menor a 15
+            // se indica al jugador que es bueno, si es 15 o más se invita al jugador a que entrene
+            // por ser malo.
+
+            // Declaración de variables y tipo Random
+            int Num, NumAl, a=0;
+            Random aleatorio = new Random();
+
+            // Asignación de valores a las variables
+            NumAl = aleatorio.Next(100);
+            Num = Convert.ToInt32(txtNum.Text);
+
+            // Ciclo do-while para captura de Num
+            do
+            {
+                if (Num < NumAl)
+                {
+                    a = a + 1;
+                    lblMensaje.Text = "El número a adivinar es MAYOR";
+                    txtNum.Focus();
+                }
+                else if (Num > NumAl)
+                {
+                    a = a + 1;
+                    lblMensaje.Text = "El número a adivinar es MENOR";
+                    txtNum.Focus();
+                }
+                else
+                {
+                    a = a + 1;
+                }
+            } while (Num != NumAl);
+
+            // Despliqegue del resultado
+            if (a <= 5)
+            {
+                lblMensaje.Text = "¡Ha adivinado el número! Usted es un buen jugador";
+                lblIntentos.Text = "Número de intentos: " + a;
+
+            }
+            else if (a >5 && a <=15)
+            {
+                lblMensaje.Text = "¡Ha adivinido el número! Usted es un jugaror regular";
+                lblIntentos.Text = "Número de intentos: " + a;
+
+            }
+            else
+            {
+                lblMensaje.Text = "¡Ha adivinado el número! Usted es un jugaor malo, lo invitamos a seguir prácticando";
+                lblIntentos.Text = "Número de intentos: " + a;
+            }
+                
+        }
+
+        private void cmdOtro_Click(object sender, EventArgs e)
+        {
+            lblMensaje.Text = "";
+            lblIntentos.Text = "";
+            txtNum.Clear();
+        }
+
+        private void cmdSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
